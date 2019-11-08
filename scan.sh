28b923e7a22124b3ff8aba62a672ecdb6ea2e02c
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-08 14:18:50 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,20 +1,20 @@
 mkdir /root/script/run
 
-rm -rf /root/run
+rm -rf /root/script/run
 
 mkdir /root/script/run
 
-curl -L github.com/chaitin/xray/releases|grep -oP "(?<=\<a\ href\=\"\/chaitin\/xray\/releases\/download\/).*?(?=\/xray\_linux\_amd64\.zip\")" > /root/run/zip.txt
+curl -L github.com/chaitin/xray/releases|grep -oP "(?<=\<a\ href\=\"\/chaitin\/xray\/releases\/download\/).*?(?=\/xray\_linux\_amd64\.zip\")" > /root/script/run/zip.txt
 
-release=`head -1 /root/run/zip.txt|tail -1`
+release=`head -1 /root/script/run/zip.txt|tail -1`
 
 head=https://github.com/chaitin/xray/releases/download/;footer=/xray_linux_amd64.zip;wget=$head$release$footer
 
-wget -P /root/run/ $wget
+wget -P /root/script/run/ $wget
 
-unzip /root/run/xray_linux_amd64.zip -d /root/run/ ; rm /root/run/xray_linux_amd64.zip
+unzip /root/script/run/xray_linux_amd64.zip -d /root/script/run/ ; rm /root/script/run/xray_linux_amd64.zip
 
-rm /root/run/zip.txt
+rm /root/script/run/zip.txt
 
 ### xray使用
 
@@ -22,7 +22,7 @@ mkdir $output/xray/
 
 xray=$output/xray/
 
-cd /root/run
+cd /root/script/run
 
 
 i=1
@@ -34,7 +34,7 @@ do
 name=.html ; txt=$head$name ;  url=$head$line
 
 mkdir /root/dir_${i}
-echo "cd /root/dir_${i} ; cp -r /root/run/* /root/dir_${i} ; ./xray_linux_amd64 webscan --url \"$line\" --json-output 1.txt ; cat 1.txt >> $output/xray/xray.txt ; echo \" \" >> $output/xray/xray.txt ; rm 1.txt ; cd ../ ; rm -r /root/dir_${i}" >> /root/exe.sh
+echo "cd /root/dir_${i} ; cp -r /root/script/run/* /root/dir_${i} ; ./xray_linux_amd64 webscan --url \"$line\" --json-output 1.txt ; cat 1.txt >> $output/xray/xray.txt ; echo \" \" >> $output/xray/xray.txt ; rm 1.txt ; cd ../ ; rm -r /root/dir_${i}" >> /root/exe.sh
 i=$((i+1))
 done
 cat /root/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 200%
@@ -49,5 +49,3 @@ cat $var > /root/script/6_port/host2ip/host.txt
 cd /root/script/6_port/host2ip ; python host2ip.py ; cat ip.txt >> /root/ip.txt ; sort -u /root/ip.txt -o /root/ip.txt ; cat ip.txt > /root/script/6_port/masnmapscan-V1.0/ip.txt ; cd /root/script/6_port/masnmapscan-V1.0 ; python masnmapcan-V1.0.py
 
 cp /root/script/6_port/masnmapscan-V1.0/scan_url_port.txt $output/masscan_detail.txt ; cp /root/script/6_port/masnmapscan-V1.0/masscan.json $output/masscan.txt ; > /root/script/6_port/masnmapscan-V1.0/scan_url_port.txt ; > /root/script/6_port/masnmapscan-V1.0/ip.txt ; > /root/script/6_port/masnmapscan-V1.0/masscan.json ; > /root/script/6_port/host2ip/host.txt ; > /root/script/6_port/host2ip/ip.txt
-
-
