37ca0a3e51a0505591def239f3acbfa47d731f92
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-10 19:11:10 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -18,27 +18,13 @@ rm /root/script/run/zip.txt
 
 ### xray使用
 
-mkdir $output/xray/
-
-xray=$output/xray/
-
 cd /root/script/run
 
 
-i=1
 for line in `cat /root/httprobe_all.txt`
-
 do
-
-
-name=.html ; txt=$head$name ;  url=$head$line
-
-mkdir /root/dir_${i}
-echo "cd /root/dir_${i} ; cp -r /root/script/run/* /root/dir_${i} ; ./xray_linux_amd64 webscan --url \"$line\" --json-output 1.txt ; cat 1.txt >> $output/xray/xray.txt ; echo \" \" >> $output/xray/xray.txt ; rm 1.txt ; cd ../ ; rm -r /root/dir_${i}" >> /root/exe.sh
-i=$((i+1))
+./xray_linux_amd64 webscan --url \"$line\" --json-output 1.txt ; cat 1.txt >> $output/xray.txt ; rm 1.txt
 done
-cat /root/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 200%
-rm -r /root/dir_* ; rm /root/exe.sh
 
 
 
@@ -46,6 +32,6 @@ rm -r /root/dir_* ; rm /root/exe.sh
 
 cat $var > /root/script/6_port/host2ip/host.txt
 
-cd /root/script/6_port/host2ip ; python host2ip.py ; cat ip.txt >> /root/ip.txt ; sort -u /root/ip.txt -o /root/ip.txt ; cat ip.txt > /root/script/6_port/masnmapscan-V1.0/ip.txt ; cd /root/script/6_port/masnmapscan-V1.0 ; python masnmapcan-V1.0.py
+cd /root/script/6_port/host2ip ; python host2ip.py ; cat ip.txt | grep "." >> /root/ip.txt ; sort -u /root/ip.txt -o /root/ip.txt ; cat ip.txt | grep "." > /root/script/6_port/masnmapscan-V1.0/ip.txt ; cd /root/script/6_port/masnmapscan-V1.0 ; python masnmapcan-V1.0.py
 
 cp /root/script/6_port/masnmapscan-V1.0/scan_url_port.txt $output/masscan_detail.txt ; cp /root/script/6_port/masnmapscan-V1.0/masscan.json $output/masscan.txt ; > /root/script/6_port/masnmapscan-V1.0/scan_url_port.txt ; > /root/script/6_port/masnmapscan-V1.0/ip.txt ; > /root/script/6_port/masnmapscan-V1.0/masscan.json ; > /root/script/6_port/host2ip/host.txt ; > /root/script/6_port/host2ip/ip.txt
