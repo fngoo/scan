0f417f3066e12229ed1ae01da83b16f5d274ccbf
fngoo <38164287+fngoo@users.noreply.github.com>
2019-12-09 22:31:43 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -24,40 +24,6 @@ cd /root/script/run
 
 date "+%Y-%m-%d_%H:%M:%S" >> /root/date.txt
 echo scan_XRAY >> /root/date.txt
-### port
-apt update
-yes|apt install nmap
-apt install --fix-missing
-
-cat $var > /root/script/6_port/host2ip/host.txt
-bash /root/script/0_subdomain/massdns/scripts/get-resolvers.sh
-cd /root/script/6_port/host2ip ; cat host.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F"A\ " '{print $2}' results.txt >> ip.txt ; rm results.txt
-cat ip.txt > /root/script/6_port/masscan_to_nmap-1/ip.txt ; sort -u /root/script/6_port/masscan_to_nmap-1/ip.txt -o /root/script/6_port/masscan_to_nmap-1/ip.txt
-
-cd /root/script/6_port/masscan_to_nmap-1 ; python scan.py
-
-grep -E ":27017 |:2181 |:6379 |:9200 |:28017 |:11211 |:21 |:22 |:25 |:53 |:110 |:123 |:137 |:143 |:161 |:389 |:445 |:512 |:873 |:1098 |:1352 |:1433 |:1521 |:2049 |:2181 |:2222 |:2601 |:3128 |:3306 |:3389 |:4100 |:4440 |:4848 |:5000 |:5422 |:5900 |:5984 |:6082 |:6379 |:7001 |:8000 |:8069 |:8080 |:8089 |:8161 |:8649 |:8980 |:9080 |:9200 |:11211 |:27017 |:50000 |:50010 |:67 |:68 " /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt > $output/6_potential.txt ; sort -u $output/6_potential.txt -o $output/6_potential.txt
-grep -E "FTP|SSH|telnet|SMTP|DNS|POP3|NTP|Samba|imap|LDAP|Https|SMB|linux r|rsync|JAVARMI|lotus|mssql|oracle|NFS|Zookeeper|DA|zebra|squid|mysql|RDP|SysBase|rundeck|glassfish|DB2|postgreSQL|vnc|CouchDB|varnish|redis|WebLogic|jdwp|zabbix|Apache|Tomcat|Nginx|Axis2|jboss|jenkins|activeMQ|ganglia|OpenNMS|websphere|elasticsearch|memcache|mongoDB|SAP|Hadoop|dhcp" /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt > $output/6_potential.txt ; sort -u $output/6_potential.txt -o $output/6_potential.txt
-
-port=`grep -E ":27017 |:2181 |:6379 |:9200 |:28017 |:11211 |:21 " /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt`
-if [ "$port" != "" ]
-then
-for ip in `cat /root/script/6_port/masscan_to_nmap-1/ip.txt`
-do
-cd /root/script/6_port/Check_Unauth
-echo $ip >> 1.txt
-python3 Check_Unauth.py $ip >> 1.txt
-grep=`grep -oP "+" 1.txt`
-if [ "$grep" = "" ]
-then
-> 1.txt
-fi
-cat 1.txt >> $output/6_unauth.txt
-> 1.txt
-done
-fi
-
-cat /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt >> /root/whatsport.txt ; cp /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt $output/6_port.txt ; > /root/script/6_port/masscan_to_nmap-1/scan_url_port.txt ; > /root/script/6_port/masscan_to_nmap-1/ip.txt ; > /root/script/6_port/masscan_to_nmap-1/scan_ip.txt ; > /root/script/6_port/masscan_to_nmap-1/masscan.json ; > /root/script/6_port/host2ip/host.txt ; > /root/script/6_port/host2ip/ip.txt
 
 cd $output
 for file in `ls | grep txt`
